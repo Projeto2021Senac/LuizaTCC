ab9032bb03873fcc83faa255d02036884bfcd8db
Guitor4 <guilhermetor10@gmail.com>
2021-10-19 11:55:50 -0300

    sla

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"><meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/jquery-ui.css">
