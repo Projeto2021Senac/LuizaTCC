ad45549fdf12e39ac044c48f12aa718abbf6f1f4
Guitor4 <guilhermetor10@gmail.com>
2021-10-24 23:29:14 -0300

    -a

--- includes/formularioListaFuncionario.php
+++ includes/formularioListaFuncionario.php
@@ -49,7 +49,7 @@
         </tbody>
 
     </table>
-    <div class="d-flex justify-content-center a">
+    <div class="d-flex justify-content-center">
                 <nav class="" aria-label="...">
                     <ul class="pagination">
                         <li class="page-item">
