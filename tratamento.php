b266fff7bb0dbf3af2ea4f8cb3fa790271f18320
Guitor4 <guilhermetor10@gmail.com>
2021-10-26 11:57:26 -0300

    autocomplete dnv

---
+++ tratamento.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use Classes\Entity\Consulta;
+use Classes\Entity\Tratamento;
+use Classes\Entity\Paciente;
+
+
+
+
+include(__DIR__ . '/includes/header.php');
+include(__DIR__ . '/includes/abrirTratamento.php');
+include(__DIR__ . '/includes/footer.php');
+
+
+?>
